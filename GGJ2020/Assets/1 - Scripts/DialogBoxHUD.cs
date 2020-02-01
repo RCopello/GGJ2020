b66cfc28db628af640d8e4debf2285ecd51bd9c7
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2020-02-01 11:30:50 -0300

    inicio HUD ink

---
+++ GGJ2020/Assets/1 - Scripts/DialogBoxHUD.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+using TMPro;
+
+public class DialogBoxHUD : MonoBehaviour
+{ 
+    public TMP_Text name;
+
+    public TMP_Text dialogPrefab;
+    public Button choicePrefab;
+
+    private TMP_Text dialog;
+    private List<Button> buttons = new List<Button>();
+
+    public void displayName(string name)
+    {
+        this.name.text = name;
+    }
+
+    public void displayText(string text)
+    {
+        dialog = Instantiate(dialogPrefab);
+        
+        this.dialog.text = text;
+    }
+
+    public void displayChoices(List<Choice> choices)
+    {
+        for (int i = 0; i < choices.Count; i++)
+        {
+            buttons.Add(Instantiate(choicePrefab));
+            buttons[i].GetComponentInChildren<Text>().text = choices[i].text;
+        }
+    }
+
+    
+}
---
+++ GGJ2020/Assets/1 - Scripts/InkController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ink.Runtime;
+using UnityEngine;
+
+public class InkController : MonoBehaviour
+{
+    public TextAsset inkJSON;
+
+    private Story story;
+    private DialogBoxHUD HUD;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Pega referencia da HUD
+        HUD = this.GetComponent<DialogBoxHUD>();
+
+        // Pega os trechos de texto do Ink
+        story = new Story(inkJSON.text);
+        displayInDialogBox("Fulano", loadStoryChunk());
+
+        if (story.currentChoices.Count > 0)
+        {
+            displayInDialogBox(story.currentChoices);
+        }
+
+        displayInDialogBox("Fulano", loadStoryChunk());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Carrega os textos do Ink
+    private string loadStoryChunk()
+    {
+        string text = "";
+
+        if (story.canContinue)
+        {
+            text = story.ContinueMaximally();
+        }
+
+        return text;
+    }
+
+    // aplica o texto e escolhas na HUD
+    private void displayInDialogBox(string name, string text)
+    {
+        HUD.displayName(name);
+        HUD.displayText(text);
+    }
+
+    private void displayInDialogBox(List<Choice> choices)
+    {
+        HUD.displayChoices(choices);
+    }
+}
